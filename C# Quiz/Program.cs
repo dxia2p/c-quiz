e13eeeff6ef8d9affbb5a96747f692a916621d10
dxia2p <71045728+dxia2p@users.noreply.github.com>
2023-01-16 09:34:52 -0700

    Add files via upload

--- C# Quiz/Program.cs
+++ C# Quiz/Program.cs
@@ -3,11 +3,11 @@
 namespace DiceRoll
 {
     class Question{
-        public static int totalAnsCorrect = 0;
-        public static int questionAmount = 0;
+        public static int totalAnsCorrect = 0; // an integer shared across all instances of this class 
+        public static int questionAmount = 0;// an integer shared across all instances of this class 
         string question = "";
-        string[] answers;
-        public Question(string q, string[] answers){ // answers must all be in lower case!
+        string[] answers; // an array with all the possible answers
+        public Question(string q, string[] answers){ 
             this.question = q;
             this.answers = answers;
             questionAmount++;
@@ -15,7 +15,7 @@ namespace DiceRoll
 
         public void askQuestion(){
             Console.WriteLine(question);
-            bool ans = CheckAnswer(Console.ReadLine());
+            bool ans = CheckAnswer(Console.ReadLine()); // returns if the users answer is correct or not
             if(ans){
                 Console.WriteLine("Answer correct!");
                 totalAnsCorrect++;
@@ -23,10 +23,10 @@ namespace DiceRoll
                 Console.WriteLine("WRONG ANSWER");
             }
         }
-        bool CheckAnswer(string a){
+        bool CheckAnswer(string a){ // function that checks the answer with all elements in answers array
             a = a.ToLower();
             for(int i = 0; i < answers.Length; i++){
-                if(answers[i] == a){
+                if(answers[i].ToLower() == a){
                     return true;
                 }
             }
@@ -38,15 +38,18 @@ namespace DiceRoll
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE TRIFLE OF TRIBES QUIZ");
+            // create the questions with question class
             Question q1 = new Question("Who is the weakest enemy?", new string[] {"goblin", "the goblin"});
             Question q2 = new Question("You lose when what building is destroyed?", new string[] {"goldstorage", "the goldstorage", "the storage of the gold", "gold storage"});
             Question q3 = new Question("What building does the most damage?", new string[] {"mageman", "mage", "the mage", "the mageman"});
             Question q4 = new Question("What building has the most range?", new string[] {"bowman", "bow", "the bow", "the bowman"});
+            // ask the questions with function in questions class
             q1.askQuestion();
             q2.askQuestion();
             q3.askQuestion();
             q4.askQuestion();
-            Console.WriteLine("You score is " + Question.totalAnsCorrect + "/" + Question.questionAmount + " (" + ((float)Question.totalAnsCorrect / (float)Question.questionAmount) * 100 + "%).");
+            Console.WriteLine("You score is " + Question.totalAnsCorrect + "/" + Question.questionAmount + 
+            " (" + ((float)Question.totalAnsCorrect / (float)Question.questionAmount) * 100 + "%)."); // convert total answers correct and questions amount to float because int will round down to 0 if divided
             if(Question.totalAnsCorrect > 2){
                 Console.WriteLine("GREAT JOB!");
             }else{
